12defab9c5e8169913f7a6e03f950c73055233fc
Erick-Nu <erick.nunez02@epn.edu.ec>
2025-01-29 09:34:52 -0500

    I SAVED THE PROJECT

--- src/VentanaIngreso.java
+++ src/VentanaIngreso.java
@@ -70,7 +70,7 @@ public class VentanaIngreso {
                 loginFrame.setContentPane(new VentanaCarros().VentanaOne);
                 loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 loginFrame.setSize(800, 600);
-                loginFrame.setPreferredSize(new Dimension(800, 600));
+                loginFrame.setPreferredSize(new Dimension(800, 400));
                 loginFrame.pack();
                 loginFrame.setVisible(true);
             }
